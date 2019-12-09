dfd239e6c8f5435fcfb8407a83876f4bc2d9d2f9
Angus Lee <officeit-dash@Anguss-MacBook-Pro.local>
2019-12-09 15:28:32 +1100

    Update installing hombrew command

--- mac-setup.sh
+++ mac-setup.sh
@@ -4,7 +4,7 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
 fi
 
 # Install my brew packages
